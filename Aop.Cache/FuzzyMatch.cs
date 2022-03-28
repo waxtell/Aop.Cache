476385a4742e103318b83711c8f0b9867c966385
Will Axtell <waxtell@transtar1.com>
2022-03-28 08:26:12 -0400

    Add summary comments

--- Aop.Cache/FuzzyMatch.cs
+++ Aop.Cache/FuzzyMatch.cs
@@ -2,16 +2,31 @@
 
 public static class It
 {
+    /// <summary>
+    /// Matches any value
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public static T IsAny<T>()
     {
         return default;
     }
 
+    /// <summary>
+    /// Matches any non-null value
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public static T IsNotNull<T>()
     {
         return default;
     }
 
+    /// <summary>
+    /// Excludes the parameter from match evaluation AND key generation
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public static T IsIgnored<T>()
     {
         return default;
