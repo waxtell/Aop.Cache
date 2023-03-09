5afa66fa2bc4f12d51615b05188eb36f6859d179
Will Axtell <waxtell@transtar1.com>
2023-03-09 16:35:37 -0500

    Add support for cache exclusion delegates (per method only)

--- Aop.Cache/IPerMethodAdapter.cs
+++ Aop.Cache/IPerMethodAdapter.cs
@@ -7,6 +7,6 @@ namespace Aop.Cache;
 
 public interface IPerMethodAdapter<T> : ICacheAdapter<T> where T : class
 {
-    IPerMethodAdapter<T> Cache<TReturn>(Expression<Func<T, Task<TReturn>>> target, Func<CacheEntryOptions> optionsFactory);
-    IPerMethodAdapter<T> Cache<TReturn>(Expression<Func<T, TReturn>> target, Func<CacheEntryOptions> optionsFactory);
+    IPerMethodAdapter<T> Cache<TReturn>(Expression<Func<T, Task<TReturn>>> target, Func<CacheEntryOptions> optionsFactory, params Func<Task<TReturn>, bool>[] exclusions);
+    IPerMethodAdapter<T> Cache<TReturn>(Expression<Func<T, TReturn>> target, Func<CacheEntryOptions> optionsFactory, params Func<TReturn, bool>[] exclusions);
 }
